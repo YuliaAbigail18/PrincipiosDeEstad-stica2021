194a63a8b826f7cb4e85ba36f6e2fee72f2b5a16
Yulia <yuliaortizcuevas@gmail.com>
2021-02-21 01:19:28 -0600

    Tarea

---
+++ TareaXX_YuliaOrtiz.R
@@ -0,0 +1,27 @@
+## Problema 1
+Pinus <- 3140
+Pinus
+Mezquite <- 1453
+Mezquite
+Teka <- 1200
+Teka
+Juiperos <- 720
+Juiperos
+Encinos <- 450
+Encinos 
+superficie<- c(Pinus, Mezquite, Teka, Juiperos, Encinos)
+barplot( superficie, col = "orange")
+mean(superficie)
+
+
+## Problema 2
+germinacion<- c(4, 1, 6, 2, 4, 2, 4, 2, 4, 6, 3, 5, 3, 2, 5, 4, 0, 5, 4,
+                2, 4, 5, 3, 5, 3, 5, 4, 3, 6, 2)
+mean(germinacion)
+sd(germinacion)
+
+## Problema 3
+Altura<- c(38, 14, 44, 11, 9, 21, 39, 28, 41, 4, 35, 24, 36, 12,
+           20, 31, 24, 25, 10, 21, 11, 36, 37, 20, 26)
+mean(Altura)
+sd(Altura)
